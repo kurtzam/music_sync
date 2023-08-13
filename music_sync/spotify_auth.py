c7024fbf3be30c0b5147e8a2eca7ff388df24d70
Aaron Kurtz <830313+kurtzam@users.noreply.github.com>
2023-08-12 23:42:34 -0700

    Spotify auth support

--- music_sync/spotify_auth.py
+++ music_sync/spotify_auth.py
@@ -1,22 +1,28 @@
 import string
 import secrets
 import hashlib
-from base64 import b64encode
+from base64 import urlsafe_b64encode
 from urllib.parse import urlparse, urlunparse, urlencode
+
+import requests
+import pkce
+
+from music_sync.models import SpotifyUserAuthRequest, SpotifyToken
 
 
 class SpotifyAuth:
     def __init__(
         self,
-        spotify_api_base: str = "https://api.spotify.com",
-        spotify_client_id: str = "1062ee1594ea467798953b9ad30c6eca",
-        redirect_uri: str = "http://localhost:5000",
+        spotify_client_id: str,
+        redirect_uri: str
     ) -> None:
-        self.spotify_api_base = spotify_api_base
+        self.spotify_api_base = "https://api.spotify.com"
         self.spotify_client_id = spotify_client_id
         self.redirect_uri = redirect_uri
+        self.scope = "user-library-read playlist-read-private"
+        self.spotify_token: SpotifyToken
 
-    def generate_random_string(self, size: int = 128) -> str:
+    def _generate_random_string(self, size: int = 128) -> str:
         """
         Generates random string of specified `size` using uppercase, lowercase,
         and digit characters.
@@ -27,21 +33,26 @@ class SpotifyAuth:
         random_string: str = "".join(secrets.choice(letters) for _ in range(size))
         return random_string
 
-    def sha256_and_b64_encode(self, b: bytes) -> bytes:
-        h = hashlib.sha256()
-        h.update(b)
-        # print(h.digest())
-        # print(h.hexdigest())
-        return b64encode(h.digest())
+    def _prepare_code_verifier_and_challenge(self):
+        code_verifier_str = self._generate_random_string(128)
+        code_verifier_digest = hashlib.sha256(code_verifier_str.encode("utf-8")).digest()
+        code_verifier_b64 = urlsafe_b64encode(code_verifier_digest)
+        code_challenge = code_verifier_b64.decode("utf-8")[:-1]
+        return {
+            "code_verifier_str": code_verifier_str,
+            "code_verifier_digest": code_verifier_digest,
+            "code_verifier_b64": code_verifier_b64,
+            "code_challenge": code_challenge,
+        }
 
-    def generate_auth_url(self, code_verifier: bytes) -> str:
-        state = self.generate_random_string(16)
-        scope = "user-library-read playlist-read-private"
-        code_challenge = self.sha256_and_b64_encode(code_verifier)
+    def generate_auth_request(self) -> SpotifyUserAuthRequest:
+        state = self._generate_random_string(16)
+        code_verifier, code_challenge = pkce.generate_pkce_pair(code_verifier_length=128)
+
         request_args = {
             "response_type": "code",
             "client_id": self.spotify_client_id,
-            "scope": scope,
+            "scope": self.scope,
             "redirect_uri": self.redirect_uri,
             "state": state,
             "code_challenge_method": "S256",
@@ -53,9 +64,24 @@ class SpotifyAuth:
         url_parts[4] = urlencode(request_args)
         auth_url = urlunparse(url_parts)
 
-        return auth_url
+        return SpotifyUserAuthRequest(
+            auth_url=auth_url,
+            state=state,
+            code_verifier=code_verifier,
+        )
 
+    def request_auth_token(self, code: str, code_verifier: str):
+        body_params = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": self.redirect_uri,
+            "client_id": self.spotify_client_id,
+            "code_verifier": code_verifier
+        }
+        resp = requests.post(
+            url="https://accounts.spotify.com/api/token",
+            data=body_params,
+        )
 
-sa = SpotifyAuth()
-code_verifier = sa.generate_random_string().encode()
-print(sa.generate_auth_url(code_verifier=code_verifier))
+        self.spotify_token = SpotifyToken.model_validate_json(resp.text)
+        return self.spotify_token
